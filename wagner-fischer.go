2df96995202c1a72a5acda808111bf2b1c2c2698
rash <felipe@xra.sh>
2014-11-26 21:14:09 -0200

    optimize wagner fischer to use only two rows

--- wagner-fischer.go
+++ wagner-fischer.go
@@ -7,32 +7,33 @@ import (
 func WagnerFischer(a, b string, icost, dcost, scost int) int {
 	lowerCost := int(math.Min(float64(icost), math.Min(float64(dcost), float64(scost))))
 
-	// allocate enough memory for the matrix
-	d := make([][]int, len(a)+1)
-	for i, _ := range d {
-		d[i] = make([]int, len(b)+1)
+	// Allocate the array that will hold the last row.
+	row1 := make([]int, len(a) + 1)
+	row2 := make([]int, len(a) + 1)
+
+	// Initialize the arrays.
+	for i := 1; i <= len(a); i++ {
+		row1[i] = i * lowerCost
 	}
 
-	// initialize the values
-	for i := 1; i <= len(a); i++ {
-		d[i][0] = i * lowerCost
-	}
 	for i := 1; i <= len(b); i++ {
-		d[0][i] = i * lowerCost
-	}
+		row2[0] = row1[0] + lowerCost
 
-	for i := 1; i <= len(a); i++ {
-		for j := 1; j <= len(b); j++ {
-			if a[i-1] == b[j-1] {
-				d[i][j] = d[i-1][j-1]
+		for j := 1; j <= len(a); j++ {
+			if a[j-1] == b[i-1] {
+				row2[j] = row1[j-1]
 			} else {
-				insertion := float64(d[i][j-1] + icost)
-				deletion := float64(d[i-1][j] + dcost)
-				substitution := float64(d[i-1][j-1] + scost)
-				d[i][j] = int(math.Min(deletion, math.Min(insertion, substitution)))
+				insertion := float64(row2[j-1] + icost)
+				deletion := float64(row1[j] + dcost)
+				substitution := float64(row1[j-1] + scost)
+				row2[j] = int(math.Min(deletion, math.Min(insertion, substitution)))
 			}
+		}
+
+		for j := 0; j < len(row1); j++ {
+			row1[j] = row2[j]
 		}
 	}
 
-	return d[len(a)][len(b)]
+	return row2[len(row2) - 1]
 }
