9a00135f60f09cb2ae321742572efde4fbec2044
rash <felipe@xra.sh>
2014-11-27 00:55:19 -0200

    reaaaaallly optimize the wagnerfischer, now the fastest algo

--- wagner-fischer.go
+++ wagner-fischer.go
@@ -1,39 +1,56 @@
 package smetrics
 
-import (
-	"math"
-)
-
 func WagnerFischer(a, b string, icost, dcost, scost int) int {
-	lowerCost := int(math.Min(float64(icost), math.Min(float64(dcost), float64(scost))))
+	var lowerCost int
+
+	// Compute the lower of the insert, deletion and substitution costs.
+	if icost < dcost && icost < scost {
+		lowerCost = icost
+	} else if (dcost < scost) {
+		lowerCost = dcost
+	} else {
+		lowerCost = scost
+	}
 
 	// Allocate the array that will hold the last row.
 	row1 := make([]int, len(a) + 1)
 	row2 := make([]int, len(a) + 1)
+	var tmp []int
 
 	// Initialize the arrays.
 	for i := 1; i <= len(a); i++ {
 		row1[i] = i * lowerCost
 	}
 
+	// For each row...
 	for i := 1; i <= len(b); i++ {
 		row2[0] = row1[0] + lowerCost
 
+		// For each column...
 		for j := 1; j <= len(a); j++ {
 			if a[j-1] == b[i-1] {
 				row2[j] = row1[j-1]
 			} else {
-				insertion := float64(row2[j-1] + icost)
-				deletion := float64(row1[j] + dcost)
-				substitution := float64(row1[j-1] + scost)
-				row2[j] = int(math.Min(deletion, math.Min(insertion, substitution)))
+				ins := row2[j-1] + icost
+				del := row1[j] + dcost
+				sub := row1[j-1] + scost
+
+				if ins < del && ins < sub {
+					row2[j] = ins
+				} else if (del < sub) {
+					row2[j] = del
+				} else {
+					row2[j] = sub
+				}
 			}
 		}
 
-		for j := 0; j < len(row1); j++ {
-			row1[j] = row2[j]
-		}
+		// Swap the rows at the end of each row.
+		tmp = row1
+		row1 = row2
+		row2 = tmp
 	}
 
-	return row2[len(row2) - 1]
+	// Because we swapped the rows, the final result is in row1 instead of row2.
+	return row1[len(row1) - 1]
 }
